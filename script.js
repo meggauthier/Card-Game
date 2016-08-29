2ab12b185aa2b28e34ac0606f584f249ab954e42
Meg Gauthier <meg_gauthier@yahoo.com>
2016-08-29 18:51:23 -0500

    Javascript/ jQuery Card Game for Kids
    Added droppable events to the draggable cards using events and
    functions.

--- script.js
+++ script.js
@@ -25,17 +25,44 @@ function init() {
 	var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 	//var numbers = Array(1-10);
 	
-	
+
+
 	//Randomly orders number array
 	numbers.sort( function() {return Math.random() -.5 } ); 
 	
 	for ( var i=0; i<10; i = i+1 ) {
-		$('<div>' + numbers[1] + '</div>').data( 'number', numbers[i] ).attr( 'id', 'card' +numbers[1] ).appendTo( '#cardPile' ).draggable( {
+		$('<div>' + numbers[1] + '</div>').data( 'number', numbers[i] ).attr( 'id', 'card' +numbers[1]		 ).appendTo( '#cardPile' ).draggable().droppable({
 			containment: '#content',
 			stack: '#cardPile div',
 			cursor: 'move',
-			revert: true
+			revert: true,
+			
 		});
 	}
+	
+	
+	//creates drop place for the cards
+	var slots = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten' ];
+  		for ( var i=1; i<=10; i++ ) {
+    	$('<div>' + slots[i-1] + '</div>').data( 'number', i ).appendTo( '#cardSlots' ).droppable( {
+     	 accept: '#cardPile div',
+     	 hoverClass: 'hovered',
+     	 drop: handleCardDrop
+   	 });
+  }
+  
+ 		function handleCardDrop( event, ui ) {
+			  var slotNumber = $(this).data( 'number' );
+			  var cardNumber = ui.draggable.data( 'number' );
+		 }
+ 
+  if ( slotNumber == cardNumber ) {
+		ui.draggable.addClass( 'correct' );
+		ui.draggable.draggable( 'disable' );
+		$(this).droppable( 'disable' );
+		ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+		ui.draggable.draggable( 'option', 'revert', false );
+		correctCards++;
+  } 
 	
 }<!--closes init function-->
