58563ebfebf33468e9f86ddcbf8006295d9e6782
Meg Gauthier <stu@ai221lab0342m17.aim.edmc.stu>
2016-09-07 17:31:58 -0500

    Updated game to include scoring.
    Used PIXI.js to implement a scoring system once all cards are matched.

--- script.js
+++ script.js
@@ -6,15 +6,16 @@ $( init );
 function init() {
 	
 	
+	
+	
 	//hide the success message
-	$('successMessage').hide().css( {
+	$('#successMessage').hide().css( {
 		left: '580px',
 		top: '250px',
 		width: 0,
 		height: 0,
 	});
 
-	
 	
 	//reset the game
 	correctCards = 0;
@@ -28,10 +29,10 @@ function init() {
 
 
 	//Randomly orders number array
-	numbers.sort( function() {return Math.random() -.5 } )
+	numbers.sort( function() {return Math.random() -.5 } );
 	
 	for ( var i=0; i<10; i = i+1 ) {
-		$('<div>' + numbers[1] + '</div>').data( 'number', numbers[i] ).attr( 'id', 'card' +numbers[1]		 ).appendTo( '#cardPile' ).draggable().droppable({
+		$('<div>' + numbers[i] + '</div>').data( 'number', numbers[i] ).attr( 'id', 'card' +numbers[i]		 ).appendTo( '#cardPile' ).draggable({
 			containment: '#content',
 			stack: '#cardPile div',
 			cursor: 'move',
@@ -49,35 +50,55 @@ function init() {
      	 hoverClass: 'hovered',
      	 drop: handleCardDrop
    	 });
-  }
+	}
+	
+}
   
- 		function handleCardDrop( event, ui ) {
-			  var slotNumber = $(this).data( 'number' );
-			  var cardNumber = ui.draggable.data( 'number' );
-		 }
+ 	function handleCardDrop( event, ui ) {
+		 var slotNumber = $(this).data( 'number' );
+		 var cardNumber = ui.draggable.data( 'number' );
+		 
  
-  if ( slotNumber == cardNumber ) {
-		ui.draggable.addClass( 'correct' );
-		ui.draggable.draggable( 'disable' );
-		$(this).droppable( 'disable' );
-		ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
-		ui.draggable.draggable( 'option', 'revert', false );
-		correctCards++;
-  } 
+  		if ( slotNumber == cardNumber ) {
+			ui.draggable.addClass( 'correct' );
+			ui.draggable.draggable( 'disable' );
+			$(this).droppable( 'disable' );
+			ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+			ui.draggable.draggable( 'option', 'revert', false );
+			correctCards++;
+ 		} 
   
- 	if (correctCards == 10 ) {
-		$('successMessage')
-		.show()
-		.fadeIn('slow')
-		.animate( {
-			left: '380px',
-			top: '200px',
-			width: '400px',
-			height: '100px',
-			opacity: 1
+ 		if (correctCards === 10 ) {
+			$('successMessage')
+			.show()
+			.fadeIn('slow')
+			.animate( {
+				left: '380px',
+				top: '200px',
+				width: '400px',
+				height: '100px',
+				opacity: 1
 			
 		});
 			
 	}
+		 this._score = 0;
+   		 this.score = new PIXI.Text('Score ' + this._score, {
+			  font: 'bold ' + Math.round(this._scale * 40) + 'px Arial',
+			  fill: '#fff',
+			  stroke: 'black',
+			  strokeThickness: Math.ceil(this._scale * 6),
+			  align: 'left'
+    });
+	
+		this.score.position.x = Math.round(this._scale * 20);
+		this.score.position.y = Math.round(this._scale * 20);
+		this.stage.addChild(this.score);
+		
+		//updates the score
+	    this._score++;
+	    this.score.setText('Score ' + this._score);
+  
+
 	
 }<!--closes init function-->
